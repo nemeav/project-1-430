2a0a50879f87b2eec16f3b6d564eeceada68bd7e
Nemesis Velazquez <nav7977@rit.edu>
2024-10-04 14:52:45 -0400

    Completed second GET request endpoint

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,4 +1,8 @@
-const genshinData = require('../data/characters.json');
+// mods
+const fs = require('fs');
+const url = require('url');
+// files
+const genshinData = JSON.parse(fs.readFileSync(`${__dirname}/../data/characters.json`));
 
 const respondJSON = (request, response, status, object) => {
   const content = JSON.stringify(object);
@@ -9,6 +13,58 @@ const respondJSON = (request, response, status, object) => {
 
   response.write(content);
   response.end();
+};
+
+// end points
+// random character generator
+const getRandom = (request, response) => {
+  const randomNum = Math.floor(Math.random() * 89);
+  const randomChar = genshinData[randomNum];
+  const responseJSON = {
+    randomChar,
+
+    message: 'This is a successful response',
+  };
+  respondJSON(request, response, 200, responseJSON);
+};
+
+// get chars by vision type
+const getVisions = (request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+  const { query } = parsedUrl;
+
+  let responseJSON;
+
+  switch (query.vision) {
+    case 'pyro':
+      responseJSON = genshinData.filter((char) => char.vision === 'Pyro');
+      break;
+    case 'hydro':
+      responseJSON = genshinData.filter((char) => char.vision === 'Hydro');
+      break;
+    case 'anemo':
+      responseJSON = genshinData.filter((char) => char.vision === 'Anemo');
+      break;
+    case 'electro':
+      responseJSON = genshinData.filter((char) => char.vision === 'Electro');
+      break;
+    case 'dendro':
+      responseJSON = genshinData.filter((char) => char.vision === 'Dendro');
+      break;
+    case 'cryo':
+      responseJSON = genshinData.filter((char) => char.vision === 'Cryo');
+      break;
+    case 'geo':
+      responseJSON = genshinData.filter((char) => char.vision === 'Geo');
+      break;
+    default:
+      responseJSON = {
+        message: 'Internal error. Something went wrong.',
+        id: 'internalError',
+      };
+      respondJSON(request, response, 400, responseJSON);
+  }
+  respondJSON(request, response, 200, responseJSON);
 };
 
 // error page
@@ -22,5 +78,7 @@ const notFound = (request, response) => {
 };
 
 module.exports = {
+  getRandom,
+  getVisions,
   notFound,
 };
--- src/server.js
+++ src/server.js
@@ -1,14 +1,54 @@
 // mods
 const http = require('http');
+const query = require('querystring');
 // files
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/getRandom': jsonHandler.getRandom,
+  '/getVisions': jsonHandler.getVisions,
+};
+
+// handles/organizes body packets
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  // error handling if anything's missing/breaks
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  // when data received, push to body array
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  // after all data collected, make obj and call func to process
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
+    handler(request, response);
+  });
+};
+
 const onRequest = (request, response) => {
   const protocol = request.connection.encrypted ? 'https' : 'http';
   const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+  // check for methods to call proper funcs
+  if (request.method === 'POST' && urlStruct[parsedUrl.pathname]) {
+    parseBody(request, response, urlStruct[parsedUrl.pathname]);
+  } else if (request.method === 'GET' && urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  } else {
+    jsonHandler.notFound(request, response);
+  }
 };
 
 // server start
